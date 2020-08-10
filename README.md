0f9f98354ac0d28655594a092f742f5d1dc7a7bd
Elijah Bobzom <ebobzom@gmail.com>
2020-08-10 01:39:07 +0100

    completed week 3 challenge

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # wejappa-task-2 (Note app without using expressjs)
 ## PLEASE  USE THESE INSTRUCTION FOR ALL QUERIES:
-        (*) All strings should be with a double quote, also all outputs are logged to the console
-        
 
-        (1) For adding of note run: node app.js add <your note title or as a word or string> <your note as a string>
+        (1) For adding of note, send a POST request with JSON body containing <title> and <note> to http://localhost:3000/notes
 
-        (2) For updating of note run: node app.js update <your previous note title as a word or string> <your note as a string>
+        (2) For updating a note, send a PUT request with JSON body containing <title> and <note> to http://localhost:3000/notes
 
-        (3) For deleting of note run: node app.js delete <your note title>
+        (3) For deleting a note, send a DELETE request to http://localhost:3000/notes/?notetitle=<YOUR NOTE TITLE>
 
-        (4) For getting of all notes run: node app.js get getnotes
+        (4) For getting of all notes make a GET request to http://localhost:3000/notes
+
+        5) For getting of one note make a GET request to http://localhost:3000/notes/?notetitle=<YOUR NOTE TITL
--- app.js
+++ app.js
@@ -1,129 +1,145 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder;
 
-function documentation(){
-    const log = `
-    PLEASE  USE THESE INSTRUCTION FOR ALL QUERIES:
-        (*) All strings should be with a double quote
+const decoder = new StringDecoder('utf-8');
+const PORT = process.env.PORT || 3000;
 
-        (1) For adding of note: node app.js add <your note title or as a word or string> <your note as a string>
-
-        (2) For updating of note: node app.js update <your previous note title as a word or string> <your note as a string>
-
-        (3) For deleting of note: node app.js delete <your note title>
-
-        (4) For getting of all notes: node app.js get getnotes
-    `
-    console.log(log);
-}
-
-let command = "";
-let value = "";
-let title = "";
-
-function parseArguments(arr){
-    if(arr.length === 3){
-        command = arr[2];
-    }
-    else if(arr.length === 4){
-        command = arr[2];
-        value = arr[3];
-    }
-    else if( arr.length === 5 ){
-        command = arr[2];
-        title = arr[3];
-        value= arr[4];
-        
-    } else{
-        documentation();
-    }
-    return {
-        command, value, title
-    }
-}
-
-let commandObj = parseArguments(process.argv);
-const userCommand = commandObj.command.toLowerCase();
-console.log('user command', userCommand)
-if( userCommand === 'add'){
-    try {
-        const note = {
-            title: commandObj.title.toLowerCase(),
-            note: commandObj.value
-        }
-        const store = fs.readFileSync('./notes/notes.txt');
-        const storeToJSON = JSON.parse(store);
-        const storeTitles = storeToJSON.notes.map((obj) => obj.title);
-
-        if(storeTitles.includes(title)){
-            console.log('Note with same title already exists, please change the title');
-            return;
-        }else{
-            storeToJSON.notes.push(note);
-            fs.writeFileSync('./notes/notes.txt', JSON.stringify(storeToJSON));
-            console.log('Note added successfully');
-            return; 
-        }
-          
-    } catch (error) {
-        console.log(error.message)
-    }
-
-    // code for first attempt
-    fs.mkdirSync('notes');
-    const note = {
-        title: commandObj.title.toLowerCase(),
-        note: commandObj.value
-    }
-    const mainDB = {
-        notes: []
-    }
-    fs.writeFileSync('./notes/notes.txt', JSON.stringify(mainDB));
-    const store = fs.readFileSync('./notes/notes.txt');
-    const storeToJSON = JSON.parse(store);
-    storeToJSON.notes.push(note);
-    fs.writeFileSync('./notes/notes.txt', JSON.stringify(storeToJSON));
-    console.log('Note added successfully');
-    return;
-
-}
-else if (userCommand === 'update'){
-    const store = fs.readFileSync('./notes/notes.txt');
-    const storeToJSON = JSON.parse(store);
-    const updatedStore = storeToJSON.note.map((obj) => {
-        if(obj.title === commandObj.title.toLowerCase()){
-            return {
-                title: obj.title,
-                note: commandObj.value
+function requestHandler(req, res){
+    const parsedUrl = url.parse(req.url, true);
+    let dataValue = '';
+    if(req.method === 'GET'){
+        if(req.method === 'GET' && parsedUrl.pathname === '/notes/'){
+            try {
+                const noteValue = fs.readFileSync(`./notes/${parsedUrl.query.notetitle}`);
+                res.writeHead( 200, {  ContentType: 'text/plain'});
+                res.end(noteValue);
+                return;
+            } catch (error) {
+                res.writeHead( 500, {  ContentType: 'text/plain'});
+                res.end('An error occured');
+                return;
             }
-        }else{
-            return obj;
         }
-    });
+        // all notes
+        if(req.method === 'GET' && parsedUrl.path === '/notes'){
+            let noteSummarry = ``;
+            fs.readdir('./notes', { encoding: 'utf-8'}, (err, filenames) => {
+                if(err){
+                    res.writeHead( 500, {  ContentType: 'text/plain'});
+                    res.end('An error occured');
+                    return;
+                }
+               
+                filenames.forEach((file, index) => {
+                    let filedata = fs.readFileSync(`./notes/${file}`, {encoding: 'utf-8'})
+                    noteSummarry += `(${index + 1}) ${filedata}\n`;
+                })
+                res.writeHead( 200, {  ContentType: 'text/plain'});
+                res.end(noteSummarry);
+                return;
+            })
+        }
 
-    fs.writeFileSync('./notes/notes.txt', JSON.stringify({ notes: updatedStore }));
-    console.log('Note updated successfully');
-    return;
-} else if(userCommand === 'delete'){
-    const store = fs.readFileSync('./notes/notes.txt');
-    const storeToJSON = JSON.parse(store);
-    const newStore = storeToJSON.filter((obj) => obj.title != commandObj.value);
+    }else{
+    
+        req.on('data', (data) =>{
+            dataValue += decoder.write(data);
+        });
 
-    fs.writeFileSync('./notes/notes.txt', JSON.stringify(newStore));
-    console.log('Note deleted successfully');
-    return;
-} else if( userCommand === 'getnotes'){
-    const store = fs.readFileSync('./notes/notes.txt');
-    const storeToJSON = JSON.parse(store);
-    let output = `
-        All Notes Bellow
-    `;
-    storeToJSON.notes.forEach((obj, i) => {
-        output += `
-            (${i + 1})
-            title: ${obj.title}
-            note: ${obj.note}
+        req.on('end', ()=> {
+            dataValue += decoder.end();
 
-        `
-    });
-    console.log(output);
+            // payload data
+            const {title, note} = JSON.parse(dataValue);
+
+            // add note route
+            if(req.method === 'POST' && parsedUrl.path === '/notes'){
+                fs.mkdir('./notes', (err) => {
+                    // if there is an error, then directory exists
+                    if(err){
+                    
+                    }
+                    
+                    // check if note with same title exists
+                    fs.readFile(`./notes/${title}`, { encoding: 'utf-8' }, (err, val) => {
+                        if(err){
+                            // if err then file does not exist
+                            fs.writeFile(`./notes/${title}`, note, (err, val) => {
+                                if(err){
+                                    res.writeHead( 500, {  ContentType: 'text/plain'});
+                                    res.end('An error occurred');
+                                    return;
+                                }
+                                res.writeHead( 200, {  ContentType: 'text/plain'});
+                                res.end('Note added successfully');
+                                return;
+                                
+                            });
+                        }else{
+                            res.writeHead( 500, {  ContentType: 'text/plain'});
+                            res.end('Note with this title already exists');
+                            return;
+                        }
+                    })
+
+                })
+            }
+            // update note route
+            if(req.method === 'PUT' && parsedUrl.path === '/notes'){
+                fs.writeFile(`./notes/${title}`, note, (err) => {
+                    if(err){
+                        res.writeHead( 500, {  ContentType: 'text/plain'});
+                        res.end('An error occured, please check your note title');
+                        return;
+                    }
+                    res.writeHead( 500, {  ContentType: 'text/plain'});
+                    res.end('Note updated successfully');
+                    return;                    
+
+                })
+            }
+    
+            // delete note route
+            if(req.method === 'DELETE' && parsedUrl.pathname === '/notes/'){
+                fs.unlink(`./notes/${parsedUrl.query.notetitle}`, (err) => {
+                    if(err){
+                        res.writeHead( 500, {  ContentType: 'text/plain'});
+                        res.end('An error occured, no file found');
+                        return;
+                    }else{
+                        res.writeHead( 200, {  ContentType: 'text/plain'});
+                        res.end('Note deleted succesfully');
+
+                        // try to delete folder if its the last note
+                        fs.rmdir('./notes', (err) => {
+                            if(err){
+                                return;
+                            }
+                            return;
+                        })
+                        return;
+                    }
+                })
+            }
+
+            // catch all route
+            if(req.path){
+                res.writeHead( 500, {  ContentType: 'text/plain'});
+                res.end('file not found');
+                return;
+            }
+        
+        });
+
+    }
+    
 }
+
+
+//create server
+const server = http.createServer(requestHandler);
+server.listen(PORT, () => {
+    console.log('server runnig on port 3000');
+})
